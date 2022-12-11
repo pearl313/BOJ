4cee4f5ac09184bbf757b637c5f04505dbd011eb
진주 <109266870+pearl313@users.noreply.github.com>
2022-12-11 19:47:42 +0900

    [Silver III] Title: 바이러스, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,29 +1,22 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
-def BFS(v):
+def DFS(v):
     global cnt
-    q = deque()
-    q.append(v)
+    cnt += 1
     visited[v] = True
-    while q:
-        cnt += 1
-        cur = q.popleft()
-        for i in graph[cur]:
-            if visited[i]:
-                continue
-            q.append(i)
-            visited[i] = True
+    for i in graph[v]:
+        if not visited[i]:
+            DFS(i)
 
 com = int(input())
 N = int(input())
 graph = [[] for _ in range(com + 1)]
 visited = [False] * (com + 1)
+cnt = 0
 for _ in range(N):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
-cnt = 0
-BFS(1)
+DFS(1)
 print(cnt - 1)
