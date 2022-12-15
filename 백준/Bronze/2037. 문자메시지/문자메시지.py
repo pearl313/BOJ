bfc4d9341e0789f1372096ec292536459f1a63b6
진주 <109266870+pearl313@users.noreply.github.com>
2022-12-15 20:42:19 +0900

    [Bronze I] Title: 문자메시지, Time: 124 ms, Memory: 114488 KB -BaekjoonHub

--- 백준/Bronze/2037. 문자메시지/문자메시지.py
+++ 백준/Bronze/2037. 문자메시지/문자메시지.py
@@ -1,22 +1,21 @@
-button = [0, ' ', '와ABC', '와DEF', '와GHI', '와JKL', '와MNO', '와PQRS', '와TUV', '와WXYZ']
+button = [0, ' ', 'ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
 
 p, w = map(int, input().split())
-message = input()
+message = list(input())
 time = 0
-
 for i in range(len(message)):
     for j in range(1, len(button)):
         if i == 0 and message[i] in button[j]:
-            time += p * (button[j].index(message[i]))
+            time += p * (button[j].index(message[i]) + 1)
             break
         else:
             if message[i] == ' ':
                 time += p
                 break
             elif message[i] in button[j] and message[i - 1] in button[j]:
-                time += p * (button[j].index(message[i])) + w
+                time += w + p * (button[j].index(message[i]) + 1)
                 break
             elif message[i] in button[j]:
-                time += p * (button[j].index(message[i]))
+                time += p * (button[j].index(message[i]) + 1)
                 break
 print(time)
