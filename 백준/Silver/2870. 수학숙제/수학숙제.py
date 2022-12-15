44bdf96de724693fe5c27c1f2f0dbc43d83c1f1f
진주 <109266870+pearl313@users.noreply.github.com>
2022-12-15 12:08:21 +0900

    [Silver IV] Title: 수학숙제, Time: 116 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Silver/2870. 수학숙제/수학숙제.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+M = int(input())
+ans = []
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+num = ''
+for i in range(M):
+    word = input().strip()
+    for j in range(len(word)):
+        if not word[j] in alphabet:
+            num += word[j]
+        elif word[j] in alphabet:
+            if len(num) == 0:
+                continue
+            elif len(num) != 1 and num[0] == '0':
+                ans.append(int(num))
+            else:
+                ans.append(int(num))
+            num = ''
+        if j == len(word) - 1 and not word[j] in alphabet and len(num) != 0:
+            if len(num) != 1 and num[0] == '0':
+                ans.append(int(num))
+            else:
+                ans.append(int(num))
+            num = ''
+ans.sort()
+for n in ans:
+    print(n)
