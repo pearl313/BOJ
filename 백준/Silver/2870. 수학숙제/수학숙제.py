10cc4ae30f28924fa3bb37fa497180541dd92eb6
진주 <109266870+pearl313@users.noreply.github.com>
2022-12-15 14:42:17 +0900

    [Silver IV] Title: 수학숙제, Time: 116 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/2870. 수학숙제/수학숙제.py
+++ 백준/Silver/2870. 수학숙제/수학숙제.py
@@ -18,6 +18,5 @@ for i in range(M):
         if j == len(word) - 1 and not word[j] in alphabet and len(num) != 0:
             ans.append(int(num))
             num = ''
-ans.sort()
-for n in ans:
+for n in sorted(ans):
     print(n)
