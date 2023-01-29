7ebb038b393b5d39d462016507e93efcee5a3f05
진주 <109266870+pearl313@users.noreply.github.com>
2023-01-29 18:34:50 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 156 ms, Memory: 117236 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,26 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def BFS(v):
+    graph[v] = 0
+    q = deque()
+    q.append(v)
+    while q:
+        cur = q.popleft()
+        if cur == K:
+            print(graph[cur])
+            break
+        for nx in (cur + 1, cur - 1, cur * 2):
+            if 0 <= nx <= 100000 and graph[nx] == -1:
+                if nx == cur * 2:
+                    graph[nx] = graph[cur]
+                    q.appendleft(nx)
+                else:
+                    graph[nx] = graph[cur] + 1
+                    q.append(nx)
+
+
+N, K = map(int, input().split())
+graph = [-1] * 100001
+BFS(N)
