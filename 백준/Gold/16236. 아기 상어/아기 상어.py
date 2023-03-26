fca0f6c1b4431e895b5d42a11a47ff2fca4499d6
진주 <109266870+pearl313@users.noreply.github.com>
2023-03-26 15:32:59 +0900

    [Gold III] Title: 아기 상어, Time: 160 ms, Memory: 116628 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -7,52 +7,53 @@ def possible(a, b):
 
 dxy = ((1, 0), (0, 1), (-1, 0), (0, -1))
 
-N = int(input())
-space = [list(map(int, input().split()))for _ in range(N)]
-size = 2
-total = 0
-cnt = 0
-for i in range(N):
-    for j in range(N):
-        if space[i][j] == 9:
-            space[i][j] = 0
-            start_i, start_j = i, j
-
-while True:
-    can_eat = []
-    compare = 10e9
-    time = 0
-    q = deque()
-    q.append((start_i, start_j, time))
+def bfs(x, y, dist):
+    global can_eat
     visited = [[False] * N for _ in range(N)]
-    visited[start_i][start_j] = True
+    q = deque()
+    q.append((x, y, dist))
+    visited[x][y] = True
+    compare = 10e9
     while q:
-        cur_i, cur_j, time = q.popleft()
-        if time > compare:
-            break
+        cur_x, cur_y, dist = q.popleft()
+        if dist > compare:
+            continue
         for dx, dy in dxy:
-            nx, ny = cur_i + dx, cur_j + dy
+            nx, ny = cur_x + dx, cur_y + dy
             if not possible(nx, ny):
                 continue
             if visited[nx][ny]:
                 continue
-            if space[nx][ny] > size:
+            if space[nx][ny] > shark_size:
                 continue
-            if space[nx][ny] and space[nx][ny] < size:
-                can_eat.append((nx, ny, time + 1))
-                compare = time
+            if space[nx][ny] < shark_size and space[nx][ny]:
+                can_eat.append((nx, ny, dist + 1))
+                compare = dist
             visited[nx][ny] = True
-            q.append((nx, ny, time + 1))
+            q.append((nx, ny, dist + 1))
 
-    if can_eat:
-        fish_i, fish_j, time = sorted(can_eat).pop(0)
-        space[fish_i][fish_j] = 0
-        start_i, start_j = fish_i, fish_j
-        cnt += 1
-        total += time
-        if cnt == size:
-            size += 1
-            cnt = 0
-    else:
-        break
-print(total)
+N = int(input())
+space = [list(map(int, input().split())) for _ in range(N)]
+shark_size = 2
+time = 0
+cnt = 0
+for i in range(N):
+    for j in range(N):
+        if space[i][j] == 9:
+            shark_i, shark_j = i, j
+            space[i][j] = 0
+can_eat = []
+bfs(shark_i, shark_j, 0)
+while can_eat:
+    fish_i, fish_j, dist = sorted(can_eat).pop(0)
+    space[fish_i][fish_j] = 0
+    time += dist
+    cnt += 1
+    shark_i, shark_j = fish_i, fish_j
+    if cnt == shark_size:
+        cnt = 0
+        shark_size += 1
+    can_eat = []
+    bfs(shark_i, shark_j, 0)
+
+print(time)
