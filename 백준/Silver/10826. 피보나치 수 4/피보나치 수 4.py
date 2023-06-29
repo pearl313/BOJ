0a853dc5b70fa361852fbfbff9185b4d49845023
진주 <109266870+pearl313@users.noreply.github.com>
2023-06-29 19:38:24 +0900

    [Silver V] Title: 피보나치 수 4, Time: 96 ms, Memory: 34916 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**7)
+
+def recur(cur):
+    if cur <= 1:
+        return cur
+    if dp[cur] != -1:
+        return dp[cur]
+    dp[cur] = recur(cur - 2) + recur(cur - 1)
+    return dp[cur]
+
+n = int(input())
+dp = [-1] * (n + 1)
+print(recur(n))
