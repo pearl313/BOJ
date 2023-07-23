c2ba85a8eb70cdf9f9f9983baca3ca780812a561
진주 <109266870+pearl313@users.noreply.github.com>
2023-07-24 03:17:13 +0900

    [Bronze IV] Title: 평균 점수, Time: 208 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int total = 0;
+        for (int i=0; i < 5; i++) {
+            int score = scanner.nextInt();
+            if (score < 40) {
+                total += 40;
+            } else {
+                total += score;
+            }
+        }
+        total /= 5;
+        System.out.println(total);
+    }
+}
