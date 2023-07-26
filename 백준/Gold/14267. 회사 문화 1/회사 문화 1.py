0181725c77993880837c71fcd3776ddfe4a57e64
진주 <109266870+pearl313@users.noreply.github.com>
2023-07-26 16:00:34 +0900

    [Gold IV] Title: 회사 문화 1, Time: 336 ms, Memory: 71208 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.py
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.py
@@ -1,22 +1,22 @@
 import sys
-sys.setrecursionlimit(100000)
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 
-def DFS(v):
-    for i in graph[v]:
-        value[i] += value[v]
-        DFS(i)
+def dfs(cur, prev):
+    for nxt in graph[cur]:
+        if nxt == prev:
+            continue
+        value[nxt] += value[cur]
+        dfs(nxt, cur)
 
 n, m = map(int, input().split())
-num = list(map(int, input().split()))
+num = [0] + list(map(int, input().split()))
 graph = [[] for _ in range(n + 1)]
-for i in range(1, n):
-    graph[num[i]].append(i + 1)
-
-value = [0 for _ in range(n + 1)]
-for i in range(m):
+for i in range(2, n + 1):
+    graph[num[i]].append(i)
+value = [0] * (n + 1)
+for _ in range(m):
     a, b = map(int, input().split())
     value[a] += b
-
-DFS(1)
+dfs(1, 0)
 print(*value[1:])
