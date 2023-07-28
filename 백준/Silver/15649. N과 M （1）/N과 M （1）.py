87c12cae5b1864581f8e70fdef6a8fb54348c618
진주 <109266870+pearl313@users.noreply.github.com>
2023-07-28 23:08:58 +0900

    [Silver III] Title: N과 M (1), Time: 208 ms, Memory: 118792 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -2,18 +2,19 @@ import sys
 input = sys.stdin.readline
 
 def recur(cur):
-    if len(selected) == M:
+    if cur == m:
         print(*selected)
         return
-    for i in range(1, N + 1):
-        if not visited[i]:
-            selected.append(i)
-            visited[i] = True
-            recur(i + 1)
-            selected.pop()
-            visited[i] = False
+    for i in range(1, n + 1):
+        if visited[i]:
+            continue
+        selected.append(i)
+        visited[i] = True
+        recur(cur + 1)
+        selected.pop()
+        visited[i] = False
 
-N, M = map(int, input().split())
+n, m = map(int, input().split())
 selected = []
-visited = [False] * (N + 1)
+visited = [False] * (n + 1)
 recur(0)
