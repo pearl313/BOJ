ef161722353c222fc6c9de052011ef5b33a53f26
진주 <109266870+pearl313@users.noreply.github.com>
2023-08-07 20:08:29 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 380 ms, Memory: 184868 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,22 +1,41 @@
 import sys
 input = sys.stdin.readline
 
-def DFS(v):
-    visited[v] = True
-    for i in graph[v]:
-        if not visited[i]:
-            DFS(i)
+def find(x):
+    # 루트인 것!
+    if parent[x] == x:
+        return x
+    else:
+        parent[x] = find(parent[x])
+        return parent[x]
 
-N, M = map(int, input().split())
-graph = [[] * (N + 1) for _ in range(N + 1)]
-visited = [False] * (N + 1)
-for _ in range(M):
-    u, v = map(int, input().split())
-    graph[u].append(v)
-    graph[v].append(u)
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a == b:
+        return
+    # 크기 비교해서 더 작은 게 큰 쪽으로 붙기
+    if rank[a] < rank[b]:
+        parent[a] = b
+    elif rank[a] > rank[b]:
+        parent[b] = a
+    else:
+        parent[a] = b
+        rank[b] += 1
+
+n, m = map(int, input().split())
+parent = list(range(1000001))
+rank = list(0 for _ in range(1000001))
+for _ in range(m):
+    a, b = map(int, input().split())
+    union(a, b)
+
+visited = [False for i in range(n + 1)]
 cnt = 0
-for i in range(1, N + 1):
-    if not visited[i]:
-        cnt += 1
-        DFS(i)
+
+for i in range(1, n + 1):
+    x = find(i)
+    if not visited[x]:
+       cnt += 1
+    visited[x] = True
 print(cnt)
