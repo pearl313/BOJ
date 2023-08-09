e4c7430ff16b7d290d9f4227a51d770d175438cf
진주 <109266870+pearl313@users.noreply.github.com>
2023-08-09 17:50:06 +0900

    [Silver I] Title: 음하철도 구구팔, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/1393. 음하철도 구구팔/음하철도 구구팔.py
+++ 백준/Silver/1393. 음하철도 구구팔/음하철도 구구팔.py
@@ -1,26 +1,27 @@
 import sys, math
 input = sys.stdin.readline
 
-def gcd(a, b):
-    while b:
-        temp = a % b
-        a = b
-        b = temp
-    return a
+xs, ys = map(int, input().split())
+xe, ye, dx, dy = map(int, input().split())
+
 
 def distance(a, b):
     return math.sqrt((a - xs) ** 2 + (b - ys) ** 2)
 
-xs, ys = map(int, input().split())
-xe, ye, dx, dy = map(int, input().split())
 
-get_gcd = gcd(dx, dy)
-dx //= get_gcd
-dy //= get_gcd
+a, b = dx, dy
+# 최대공약수 구하기
+while b:
+    temp = a % b
+    a = b
+    b = temp
 
+dx //= a
+dy //= a
+    
 x, y = xe, ye
 # 가능한 모든 점 넣어서 거리 확인해보기
-while distance(x, y) > distance(x + dx, y + dy):
+while distance(x, y) > distance(x + dx, y + dy):    
     x += dx
     y += dy
 print(x, y)
