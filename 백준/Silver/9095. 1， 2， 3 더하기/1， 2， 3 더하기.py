07dd0891257a631258c8f105868642ee17a4adb8
진주 <109266870+pearl313@users.noreply.github.com>
2023-08-09 22:13:24 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,22 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-def recur(cur):
-    global ans
-    if sum(selected) > n:
-        return
-    if sum(selected) == n:
-        ans += 1
-        return
+def recur(total):
+    if total == n:
+        return 1
+    if total > n:
+        return 0
+    if dp[total] != -1:
+        return dp[total]
+    if dp[total] == -1:
+        dp[total] = 0
     for i in range(1, 4):
-        selected.append(i)
-        recur(cur + 1)
-        selected.pop()
+        dp[total] += recur(total + i)
+    return dp[total]
 
 t = int(input())
 for _ in range(t):
     n = int(input())
-    selected = []
-    ans = 0
-    recur(1)
-    print(ans)
+    dp = [-1] * (n + 1)
+    recur(0)
+    print(dp[0])
