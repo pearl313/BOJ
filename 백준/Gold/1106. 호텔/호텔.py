fe11db8b662a756e69cdc5ee14499b988e551a49
진주 <109266870+pearl313@users.noreply.github.com>
2023-10-05 17:42:29 +0900

    [Gold V] Title: 호텔, Time: 128 ms, Memory: 115848 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.py
+++ 백준/Gold/1106. 호텔/호텔.py
@@ -1,18 +1,18 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10000000)
 
 def recur(cur_p):
     if cur_p >= c:
         return 0
     if dp[cur_p] != -1:
         return dp[cur_p]
-    dp[cur_p] = 1e10
+    dp[cur_p] = 9999999999
     for i in range(n):
-        dp[cur_p] = min(recur(cur_p + city[i][1]) + city[i][0], dp[cur_p])
+        dp[cur_p] = min(dp[cur_p], recur(cur_p + city[i][1]) + city[i][0])
     return dp[cur_p]
+
 
 c, n = map(int, input().split())
 city = [list(map(int, input().split())) for _ in range(n)]
-dp = [-1] * 1001
+dp = [-1] * 1111
 print(recur(0))
