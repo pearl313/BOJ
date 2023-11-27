ffa8bf97c206815bd9da1762338e6b54090cdf68
진주 <109266870+pearl313@users.noreply.github.com>
2023-11-27 23:25:49 +0900

    [Silver I] Title: 2-SAT - 1, Time: 1032 ms, Memory: 125224 KB -BaekjoonHub

--- 백준/Silver/11277. 2－SAT － 1/2－SAT － 1.py
+++ 백준/Silver/11277. 2－SAT － 1/2－SAT － 1.py
@@ -4,14 +4,11 @@ input = sys.stdin.readline
 def recur(cur):
     global ans
     if cur == n:
-        # print(*select)
-        # ans.add(tuple(select))
         x = []
         for j in range(n):
             x.append(True if select[j] else False)
         total = []
         for k in range(m):
-            # print(k, cal[k], cal, abs(cal[k][0]), abs(cal[k][1]), x)
             x1, x2 = x[abs(cal[k][0]) - 1], x[abs(cal[k][1]) - 1]
             if cal[k][0] < 0:
                 x1 = not x1
@@ -20,12 +17,10 @@ def recur(cur):
             if not (x1 or x2):
                 return
             total.append(x1 or x2)
-        # print(total)
         f = total.pop(0)
         while total:
             f = f and total[0]
             total.pop(0)
-        # print('->', f, total)
         if f:
             print(1)
             exit()
