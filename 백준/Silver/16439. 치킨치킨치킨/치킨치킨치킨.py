eed0715a505504a612dead8a096808755a295a24
진주 <109266870+pearl313@users.noreply.github.com>
2023-12-22 16:11:48 +0900

    [Silver IV] Title: 치킨치킨치킨, Time: 152 ms, Memory: 110768 KB -BaekjoonHub

--- 백준/Silver/16439. 치킨치킨치킨/치킨치킨치킨.py
+++ 백준/Silver/16439. 치킨치킨치킨/치킨치킨치킨.py
@@ -1,7 +1,7 @@
 import sys
 input = sys.stdin.readline
 
-def recur(cur):
+def recur(cur, start):
     global ans
     if cur == 3:
         total = 0
@@ -9,18 +9,14 @@ def recur(cur):
             total += max(arr[j][selected[0]], arr[j][selected[1]], arr[j][selected[2]])
         ans = max(ans, total)
         return
-    for i in range(m):
-        if not visited[i]:
-            selected.append(i)
-            visited[i] = True
-            recur(cur + 1)
-            selected.pop()
-            visited[i] = False
+    for i in range(start, m):
+        selected.append(i)
+        recur(cur + 1, i + 1)
+        selected.pop()
 
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 selected = []
-visited = [False] * m
 ans = 0
-recur(0)
+recur(0, 0)
 print(ans)
