a10ff9dbb45c95d6486269236656f37a9e2c0057
진주 <109266870+pearl313@users.noreply.github.com>
2024-01-04 12:25:26 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.py
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.py
@@ -5,12 +5,12 @@ n = int(input())
 cnt = 0
 for x in range(1, 101):
     for y in range(1, 101):
-        for z in range(1, 101):
-            if x + y + z != n:
-                continue
-            if z < y + 2:
-                continue
-            if x % 2 == 1:
-                continue
-            cnt += 1
+        z = n - x - y
+        if x + y + z != n:
+            continue
+        if z < y + 2:
+            continue
+        if x % 2 == 1:
+            continue
+        cnt += 1
 print(cnt)
