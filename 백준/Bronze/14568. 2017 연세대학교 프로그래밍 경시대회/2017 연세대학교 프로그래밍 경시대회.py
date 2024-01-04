b862c8bf27b5fa2e372dd5c3b8313f89fbbef055
진주 <109266870+pearl313@users.noreply.github.com>
2024-01-04 12:23:43 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 120 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+cnt = 0
+for x in range(1, 101):
+    for y in range(1, 101):
+        for z in range(1, 101):
+            if x + y + z != n:
+                continue
+            if z < y + 2:
+                continue
+            if x % 2 == 1:
+                continue
+            cnt += 1
+print(cnt)
