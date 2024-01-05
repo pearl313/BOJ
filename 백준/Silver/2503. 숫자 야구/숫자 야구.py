768c126dc84f5e2a3ac5bd476bdefaa6ae5741c1
진주 <109266870+pearl313@users.noreply.github.com>
2024-01-05 15:33:13 +0900

    [Silver III] Title: 숫자 야구, Time: 128 ms, Memory: 110536 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.py
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -1,32 +1,31 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
 candidate = []
-for i in range(100, 999):
-    if '0' in list(str(i)) or str(i)[0] == str(i)[1] or str(i)[1] == str(i)[2] or str(i)[0] == str(i)[2]:
+for i in range(123, 988):
+    num = str(i)
+    if '0' in num:
         continue
-    else:
-        candidate.append(str(i))
-ans = []
-for _ in range(N):
-    num, strike, ball = map(int, input().split())
-
-    for i in candidate:         # 후보군들 순회하면서
-        cnt_s = 0               # 스트라이크 개수
-        cnt_b = 0               # 볼 개수
-        for j in range(3):
-            if i[j] in str(num):
-                if i[j] == str(num)[j]:      # 입력받은 스트라이크 있으면서 후보군의 각 숫자의 위치 + 값 == 입력받은 수의 각 숫자의 위치 + 값
-                    cnt_s += 1                          # 스트라이크 개수 업
-                if i[j] != str(num)[j]:  # 후보군의 각 숫자의 위치 + 값 != 입력받은 수의 각 숫자의 위치 + 값 이지만, 그 숫자가 후보군 안에 존재하면
-                    cnt_b += 1                          # 볼 개수 업
-        if cnt_s == strike and cnt_b == ball:
-            ans.append(i)
-
-    candidate = ans
-    ans = []
-# print(candidate)
-print(len(candidate))
-
-
+    if num[0] == num[1] or num[1] == num[2] or num[0] == num[2]:
+        continue
+    candidate.append(i)
+n = int(input())
+questions = [list(map(int, input().split())) for _ in range(n)]
+for num, strick, ball in questions:
+    possible = []
+    for can in candidate:
+        num = str(num)
+        can = str(can)
+        s, b = 0, 0
+        for i in range(3):
+            # 스트라이크 체크
+            if num[i] == can[i]:
+                s += 1
+            else:
+                # 볼 체크
+                if num[i] in can:
+                    b += 1
+        if s == strick and b == ball:
+            possible.append(can)
+    candidate = possible[::]
+print(len(possible))
