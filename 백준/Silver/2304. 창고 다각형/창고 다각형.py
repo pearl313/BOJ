c1b39492eba699a7e8ff9391061dea0c4b1da2e9
진주 <109266870+pearl313@users.noreply.github.com>
2024-01-08 09:55:47 +0900

    [Silver II] Title: 창고 다각형, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.py
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -1,37 +1,30 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-region = [0] * 1001
-last = 0
-first = 0
-for _ in range(N):
-    L, H = map(int, input().split())
-    region[L] = H
-    last = max(L, last)
-    first = max(H, first)
-first_idx = region.index(first)
-val = []
-# 제일 높은 거 처음으로 나올 때까지 / 그 이후, 이렇게 나눠서 해보기
-for i in range(first_idx + 1):
-    # 리스트 비어있으면,
-    if not val:
-        if not region[i]:
-            continue
-        else:
-            val.append(region[i])
+'''
+1. 가장 큰 기둥을 찾아야 함
+2. 가장 큰 기둥을 만나기 전까지는 한 칸씩 이동하면서 높이 갱신
+3. 뒤에서부터 큰 기둥을 만나기 전까지 높이 갱신 진행
+4. 그 값을 다 더해줌
+'''
 
-    # 리스트에 값이 있으면,
-    else:
-        if region[i] > val[-1]:
-            val.append(region[i])
-        else:
-            val.append(val[-1])
-
-# 제일 높은 층 한 개인 경우,
-for i in range(first_idx + 1, last + 1):
-    second = max(region[i:])
-    if region[i] <= second:
-        val.append(second)
-
-print(sum(val))
+n = int(input())
+ls = [list(map(int, input().split())) for _ in range(n)]
+ls.sort(key=lambda x:x[0])
+max_ls = max(ls, key=lambda x:x[1])
+height = ls[0][1]
+area = max_ls[1]
+# 앞에서부터 큰 기둥까지
+for i in range(1, ls.index(max_ls) + 1):
+    area += height * (ls[i][0] - ls[i - 1][0])
+    if height > ls[i][1]:
+        continue
+    height = ls[i][1]
+height = ls[-1][1]
+# 뒤에서부터 큰 기둥까지
+for i in range(n - 2, ls.index(max_ls) - 1, -1):
+    area += height * (ls[i + 1][0] - ls[i][0])
+    if height > ls[i][1]:
+        continue
+    height = ls[i][1]
+print(area)
