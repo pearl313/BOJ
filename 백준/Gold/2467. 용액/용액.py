306ee4351a5aea67b7edcfcb3476a795ac195e05
진주 <109266870+pearl313@users.noreply.github.com>
2024-01-20 18:52:23 +0900

    [Gold V] Title: 용액, Time: 140 ms, Memory: 126196 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -1,21 +1,26 @@
 import sys
 input = sys.stdin.readline
-
-N = int(input())
-ls = list(map(int, input().split()))
-s = 0
-e = N - 1
+'''
+더한 값을 바로 절대값 처리하지 말고,
+양수인지 음수인지에 따라서 포인터의 위치 바꿔줌 
+'''
+n = int(input())
+solutions = sorted(map(int, input().split()))
+s, e = 0, (n - 1)
 ans = 1e10
+ans1, ans2 = 0, 0
 while s < e:
-    temp = ls[e] + ls[s]
-    if ans > abs(temp):
-        ans_1, ans_2 = ls[s], ls[e]
-        ans = abs(temp)
-    elif temp == 0:
-        ans_1, ans_2 = ls[s], ls[e]
+    temp = solutions[s] + solutions[e]
+    if temp == 0:
+        ans1 = solutions[s]
+        ans2 = solutions[e]
         break
+    elif abs(temp) < ans:
+        ans = abs(temp)
+        ans1 = solutions[s]
+        ans2 = solutions[e]
     elif temp > 0:
         e -= 1
     else:
         s += 1
-print(ans_1, ans_2)
+print(ans1, ans2)
