0422460e20c23c86ddff32e34638fed8d2224ce9
진주 <109266870+pearl313@users.noreply.github.com>
2024-01-27 16:17:54 +0900

    [Gold IV] Title: 나는 정말 휘파람을 못 불어, Time: 2180 ms, Memory: 128704 KB -BaekjoonHub

---
+++ 백준/Gold/24956. 나는 정말 휘파람을 못 불어/나는 정말 휘파람을 못 불어.py
@@ -0,0 +1,18 @@
+import sys
+from collections import defaultdict
+input = sys.stdin.readline
+
+n = int(input())
+s = ['a'] + list(input().strip())
+prefix = [0] * (n + 1)
+suffix = [0] * (n + 2)
+for i in range(1, n + 1):
+    prefix[i] = prefix[i - 1] + (1 if s[i] == 'W' else 0)
+for i in range(n, 0, -1):
+    suffix[i] = suffix[i + 1] + (1 if s[i] == 'E' else 0)
+ans = 0
+for i in range(1, n + 1):
+    if s[i] == 'H':
+        ans += prefix[i] * (2 ** suffix[i] - suffix[i] - 1)
+        ans %= 1000000007
+print(ans)
