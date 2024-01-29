21e1e1666484d4f282b15ad1949f1d3f4ded0abd
진주 <109266870+pearl313@users.noreply.github.com>
2024-01-29 15:40:28 +0900

    [Gold V] Title: 빗물, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -1,10 +1,17 @@
-H, W = map(int, input().split())
-block = list(map(int, input().split()))
-total = 0
+import sys
+input = sys.stdin.readline
 
-for i in range(1, W - 1):
-    left = max(block[:i])
-    right = max(block[i + 1:])
-    if min(left, right) > block[i]:
-        total += min(left, right) - block[i]
-print(total)
+h, w = map(int, input().split())
+ls = [0] + list(map(int, input().split()))
+max_idx = ls.index(max(ls))
+
+new_ls = ls[::]
+for i in range(1, max_idx + 1):
+    new_ls[i] = new_ls[i] if new_ls[i] > new_ls[i - 1] else new_ls[i - 1]
+for i in range(w - 1, max_idx, -1):
+    new_ls[i] = new_ls[i] if new_ls[i] > new_ls[i + 1] else new_ls[i + 1]
+ans = 0
+for i in range(1, w + 1):
+    if new_ls[i] > ls[i]:
+        ans += new_ls[i] - ls[i]
+print(ans)
