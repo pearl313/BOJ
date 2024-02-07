c71b7871619686d5cfaa7aad534ebf32e37fcd62
진주 <109266870+pearl313@users.noreply.github.com>
2024-02-07 16:04:10 +0900

    [Silver II] Title: Three Dots, Time: 1308 ms, Memory: 111540 KB -BaekjoonHub

--- 백준/Silver/13423. Three Dots/Three Dots.py
+++ 백준/Silver/13423. Three Dots/Three Dots.py
@@ -1,14 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-T = int(input())
-for _ in range(T):
-    N = int(input())
-    num = sorted(map(int, input().split()))
-    num_dict = {n: True for n in num}
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    ls = sorted(map(int, input().split()))
     cnt = 0
-    for i in range(N):
-        for j in range(i + 1, N):
-            if (num[i] + num[j]) / 2 in num_dict:
-                cnt += 1
+    for i in range(n):
+        for j in range(i + 1, n):
+            dist = (ls[i] + ls[j]) / 2
+            s, e = i, j
+            while s <= e:
+                mid = (s + e) // 2
+                if ls[mid] == dist:
+                    cnt += 1
+                    break
+                elif ls[mid] < dist:
+                    s = mid + 1
+                else:
+                    e = mid - 1
     print(cnt)
