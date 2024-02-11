b019bd718596d2bb27660d9466dcad449277c100
진주 <109266870+pearl313@users.noreply.github.com>
2024-02-11 23:25:56 +0900

    [Gold V] Title: 수 나누기 게임, Time: 380 ms, Memory: 144808 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.py
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.py
@@ -1,16 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-x = list(map(int, input().rstrip().split()))
-S = set(x)
-M = max(x)
-sieve = [0 for _ in range(M+1)]
-for i in x:
-    if i == M: continue
-    for j in range(2*i, M+1, i):
-        if j in S:
-            sieve[i] += 1
-            sieve[j] -= 1
-for i in x:
-    print(sieve[i], end = ' ')
+n = int(input())
+players = list(map(int, input().split()))
+check = set(players)
+max_val = max(players)
+points = [0] * (max_val + 1)
+for i in range(n):
+    if players[i] == max_val:
+        continue
+    for j in range(2 * players[i], max_val + 1, players[i]):
+        if j in check:
+            points[players[i]] += 1
+            points[j] -= 1
+for i in players:
+    print(points[i], end=' ')
