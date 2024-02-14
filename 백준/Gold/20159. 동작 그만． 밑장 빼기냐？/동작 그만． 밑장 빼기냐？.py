e7e0f42394432581a87b752a877d4f5a815460b3
진주 <109266870+pearl313@users.noreply.github.com>
2024-02-14 17:18:00 +0900

    [Gold IV] Title: 동작 그만. 밑장 빼기냐?, Time: 136 ms, Memory: 120400 KB -BaekjoonHub

--- 백준/Gold/20159. 동작 그만． 밑장 빼기냐？/동작 그만． 밑장 빼기냐？.py
+++ 백준/Gold/20159. 동작 그만． 밑장 빼기냐？/동작 그만． 밑장 빼기냐？.py
@@ -1,29 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-arr = list(map(int, input().split()))
-
-# 밑장 빼기를 안했을 때 정훈이 카드의 합
-junghun_origin_sum = 0
-for i in range(N):
+n = int(input())
+cards = list(map(int, input().split()))
+time = n // 2
+total = 0
+for i in range(n):
     if i % 2 == 0:
-        junghun_origin_sum += arr[i]
-ans = junghun_origin_sum
-junghun_sum = junghun_origin_sum
-
-# (정훈이 차례에서) 맨 아래에서부터 밑장 빼기 했을 때 합 어떤지
-for i in range(N-1, 0, -2):
-    junghun_sum += arr[i]
-    junghun_sum -= arr[i-1]
-    ans = max(ans, junghun_sum)
-
-
-junghun_sum = junghun_origin_sum
-# 상대 차례에서 맨 아래부터 밑장 빼기를 했을 때 합이 어떤지
-for i in range(N-2, 1, -2):
-    junghun_sum -= arr[i]
-    junghun_sum += arr[i-1]
-    ans = max(ans, junghun_sum)
-
+        total += cards[i]
+ans = total
+temp = total
+for i in range(n - 1, 0, -2):
+    temp += cards[i]
+    temp -= cards[i - 1]
+    ans = max(ans, temp)
+temp = total
+for i in range(n - 2, 1, -2):
+    temp -= cards[i]
+    temp += cards[i - 1]
+    ans = max(ans, temp)
 print(ans)
