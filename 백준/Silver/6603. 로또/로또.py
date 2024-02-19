083cc3f44d7e22649c995322ac6024fb317a72be
진주 <109266870+pearl313@users.noreply.github.com>
2024-02-20 01:12:41 +0900

    [Silver II] Title: 로또, Time: 156 ms, Memory: 111880 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -1,21 +1,21 @@
 import sys
 input = sys.stdin.readline
 
-def recur(cur, start):
-    if cur == 6:
-        print(*selected)
-    for i in range(start, len(s)):
-        selected.append(s[i])
-        recur(cur + 1, i + 1)
-        selected.pop()
-
 while True:
-    test_case = list(map(int, input().split()))
-    if test_case == [0]:
-        exit()
-    else:
-        k = test_case[0]
-        s = test_case[1:]
-        selected = []
-        recur(0, 0)
+    ls = list(map(int, input().split()))
+    if ls == [0]:
+        break
+    k = ls[0]
+    nums = sorted(ls[1:])
+    arr = [0 for _ in range(6)]
+
+    def recur(cur, start):
+        if cur == 6:
+            print(*arr)
+            return
+        for i in range(start, k):
+            arr[cur] = nums[i]
+            recur(cur + 1, i + 1)
+
+    recur(0, 0)
     print()
