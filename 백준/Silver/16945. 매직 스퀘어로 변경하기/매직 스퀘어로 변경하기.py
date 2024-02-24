31d09a361617f3eb105d7690c1ac3928e456bfd2
진주 <109266870+pearl313@users.noreply.github.com>
2024-02-24 21:51:49 +0900

    [Silver II] Title: 매직 스퀘어로 변경하기, Time: 372 ms, Memory: 115180 KB -BaekjoonHub

--- 백준/Silver/16945. 매직 스퀘어로 변경하기/매직 스퀘어로 변경하기.py
+++ 백준/Silver/16945. 매직 스퀘어로 변경하기/매직 스퀘어로 변경하기.py
@@ -1,49 +1,58 @@
 import sys
 input = sys.stdin.readline
 
-def check(ls):
-    hap = sum(ls[0])
-    for i in range(3):
-        if sum(ls[i]) != hap:
-            return False
-        col = 0
-        for j in range(3):
-            col += ls[j][i]
-        if col != hap:
-            return False
-    if ls[0][0] + ls[1][1] + ls[2][2] != hap:
-        return False
-    if ls[2][0] + ls[1][1] + ls[0][2] != hap:
-        return False
-    return True
-
-def cost(ls):
-    global arr
-    total = 0
-    for i in range(3):
-        for j in range(3):
-            if arr[i][j] != ls[i][j]:
-                total += abs(arr[i][j] - ls[i][j])
-    return total
-
-def recur(cur):
-    global ans
-    if cur == 9:
-        ls = [selected[:3], selected[3:6], selected[6:]]
-        if check(ls):
-            ans = min(ans, cost(ls))
-        return
-    for i in range(1, 10):
-        if not visited[i]:
-            visited[i] = True
-            selected.append(i)
-            recur(cur + 1)
-            visited[i] = False
-            selected.pop()
-
 arr = [list(map(int, input().split())) for _ in range(3)]
-selected = []
+arr2 = [[0] * 3 for _ in range(3)]
 visited = [False] * 10
 ans = 1e10
-recur(0)
+
+def check():
+    # 행 확인
+    for i in range(3):
+        total = 0
+        for j in range(3):
+            total += arr2[i][j]
+        # 각 행의 합이 모두 같아야 하므로 15가 아니면 안됨.
+        if total != 15:
+            return False
+
+    # 열 확인
+    for j in range(3):
+        total = 0
+        for i in range(3):
+            total += arr2[i][j]
+        if total != 15:
+            return False
+
+    # 대각선 확인
+    if arr2[0][0] + arr2[1][1] + arr2[2][2] != 15:
+        return False
+    if arr2[0][2] + arr2[1][1] + arr2[2][0] != 15:
+        return False
+
+    return True
+
+def recur(x, y, cost):
+    global ans
+    # 열 끝까지 가면, 다음 줄로 이동
+    if y == 3:
+        x += 1
+        y = 0
+    # 행 끝까지 가면,
+    if x == 3:
+        if not check():
+            return
+        ans = min(ans, cost)
+        return
+    # 1부터 10까지 하나씩 다 넣어서 바꿔보기 (순열)
+    for i in range(1, 10):
+        if visited[i]:
+            continue
+        visited[i] = True
+        arr2[x][y] = i
+        # 비용은 원래 배열에서 바꿔준 숫자와의 차이
+        recur(x, y + 1, cost + abs(arr[x][y] - i))
+        visited[i] = False
+
+recur(0, 0, 0)
 print(ans)
