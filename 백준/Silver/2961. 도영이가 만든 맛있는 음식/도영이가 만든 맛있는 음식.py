32410ea7f7d6a138f6eb578a3c8cfd3b50828866
진주 <109266870+pearl313@users.noreply.github.com>
2024-02-24 21:08:48 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
@@ -1,19 +1,19 @@
-import sys, itertools
+import sys
 input = sys.stdin.readline
 
-N = int(input())
-ingredients = [list(map(int, input().split())) for _ in range(N)]
-arr = list(range(N))
-n = 1
-ans = float('inf')
-while n <= N:
-    for i in itertools.combinations(arr, n):
-        sour = 1
-        bitter = 0
-        for j in i:
-            sour *= ingredients[j][0]
-            bitter += ingredients[j][1]
-        temp = abs(sour - bitter)
-        ans = min(ans, temp)
-    n += 1
+n = int(input())
+taste = [list(map(int, input().split())) for _ in range(n)]
+ans = 1e10
+
+def recur(cur, cnt, total_sour, total_bitter):
+    global ans
+    if cur == n:
+        if cnt > 0:
+            ans = min(ans, abs(total_sour - total_bitter))
+        return
+
+    recur(cur + 1, cnt + 1, total_sour * taste[cur][0], total_bitter + taste[cur][1])
+    recur(cur + 1, cnt, total_sour, total_bitter)
+
+recur(0, 0, 1, 0)
 print(ans)
