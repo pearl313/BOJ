037562b66b49b8898305a4012275d3aaf42dae5a
진주 <109266870+pearl313@users.noreply.github.com>
2024-02-28 01:33:38 +0900

    [Silver I] Title: 단풍잎 이야기, Time: 480 ms, Memory: 111552 KB -BaekjoonHub

--- 백준/Silver/16457. 단풍잎 이야기/단풍잎 이야기.py
+++ 백준/Silver/16457. 단풍잎 이야기/단풍잎 이야기.py
@@ -1,29 +1,30 @@
 import sys
 input = sys.stdin.readline
 
-def recur(cur, cnt):
-    global ans, selected, temp
-    if cur == n * 2 + 1:
-        if cnt == n:
-            temp = 0
-            for i in range(m):
-                temp_temp = 0
-                for j in range(k):
-                    if skills[i][j] in selected_key:
-                        temp_temp += 1
-                if temp_temp == k:
-                    temp += 1
-            ans = max(ans, temp)
-            return
-    else:
-        selected_key.append(cur)
-        recur(cur + 1, cnt + 1)
-        selected_key.pop()
-        recur(cur + 1, cnt)
-
 n, m, k = map(int, input().split())
-skills = [list(map(int, input().split())) for _ in range(m)]
-selected_key = []
-ans, temp = 0, 0
+ls = [list(map(int, input().split())) for _ in range(m)]
+key = []
+
+def recur(cur, cnt):
+    global ans
+    if cur == 2 * n + 1:
+        if cnt == n:
+            q = 0
+            for i in ls:
+                temp = 0
+                for j in range(k):
+                    if i[j] in key:
+                        temp += 1
+                if temp == k:
+                    q += 1
+            ans = max(ans, q)
+        return
+
+    key.append(cur)
+    recur(cur + 1, cnt + 1)
+    key.pop()
+    recur(cur + 1, cnt)
+
+ans = -1e10
 recur(1, 0)
 print(ans)
