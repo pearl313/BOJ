559f8e5dbbe5580bb453ff7cc762dd32e19e1838
진주 <109266870+pearl313@users.noreply.github.com>
2024-02-28 01:17:34 +0900

    [Gold IV] Title: 다이어트, Time: 204 ms, Memory: 114820 KB -BaekjoonHub

--- 백준/Gold/19942. 다이어트/다이어트.py
+++ 백준/Gold/19942. 다이어트/다이어트.py
@@ -1,29 +1,38 @@
 import sys
 input = sys.stdin.readline
 
-def recur(cur, protein, fat, carbo, vita, cost):
-    global ans, ans_ls
-    if cur == n:
-        if protein >= min_val[0] and fat >= min_val[1] and carbo >= min_val[2] and vita >= min_val[3]:
-            if cost < ans:
-                ans = cost
-                ans_ls = [[i + 1 for i in selected]]
-            elif cost == ans:
-                ans_ls.append([i + 1 for i in selected])
-        return
-    selected.append(cur)
-    recur(cur + 1, protein + ingredients[cur][0], fat + ingredients[cur][1], carbo + ingredients[cur][2],
-          vita + ingredients[cur][3], cost + ingredients[cur][4])
-    selected.pop()
-    recur(cur + 1, protein, fat, carbo, vita, cost)
-
 n = int(input())
-min_val = list(map(int, input().split()))
-ingredients = [list(map(int, input().split())) for _ in range(n)]
+minimum = list(map(int, input().split()))
+info = [list(map(int, input().split())) for _ in range(n)]
+
 ans = 1e10
 ans_ls = []
-selected = []
-recur(0, 0, 0, 0, 0, 0)
+def recur(cur, choose, protein, fat, carbo, vita, cost):
+    global ans, ans_ls
+    if cur == n:
+        if protein >= minimum[0] and fat >= minimum[1] and carbo >= minimum[2] and vita >= minimum[3]:
+            if ans > cost:
+                ans = cost
+                temp = []
+                for i in choose.split(' '):
+                    if i == '':
+                        continue
+                    temp.append(int(i) + 1)
+                ans_ls = [temp]
+            elif ans == cost:
+                temp = []
+                for i in choose.split(' '):
+                    if i == '':
+                        continue
+                    temp.append(int(i) + 1)
+                ans_ls.append(temp)
+        return
+
+    recur(cur + 1, choose + str(cur) + ' ', protein + info[cur][0], fat + info[cur][1], carbo + info[cur][2], vita + info[cur][3], cost + info[cur][4])
+    recur(cur + 1, choose, protein, fat, carbo, vita, cost)
+
+recur(0, '', 0, 0, 0, 0, 0)
+
 if ans == 1e10:
     print(-1)
 else:
