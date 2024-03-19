43f23b9d3358932ad3ce669eecbd8d76a55787e7
진주 <109266870+pearl313@users.noreply.github.com>
2024-03-19 22:34:21 +0900

    [Silver III] Title: 계단 오르기, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -2,19 +2,20 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-stairs = [0] + list(int(input()) for _ in range(n))
-dp = [[0, 0] for _ in range(n + 1)]
+ls = [0] + list(int(input()) for _ in range(n))
+
+# 현재 꺼만, 이전 꺼 + 현재 꺼
+dp = [(0, 0) for _ in range(n + 1)]
 
 if n == 1:
-    print(stairs[1])
+    print(ls[1])
 elif n == 2:
-    print(stairs[1] + stairs[2])
+    print(ls[1] + ls[2])
 else:
-    dp[0] = [0, 0]
-    dp[1] = [stairs[1], 0]
-    dp[2] = [stairs[1] + stairs[2], stairs[2]]
+    dp[1] = (ls[0], ls[0] + ls[1])
+    dp[2] = (ls[2], ls[1] + ls[2])
 
     for i in range(3, n + 1):
-        dp[i] = [dp[i - 1][1] + stairs[i], max(dp[i - 2]) + stairs[i]]
-    
+        # 연속 3개면 안되니까 두번째 앞선 값들 중에서 최댓값 + 현재 위치 값 또는 바로 앞전 값들 중 앞전 꺼만 더한 값에 + 현재 위치 값
+        dp[i] = (max(dp[i - 2]) + ls[i], dp[i - 1][0] + ls[i])
     print(max(dp[n]))
