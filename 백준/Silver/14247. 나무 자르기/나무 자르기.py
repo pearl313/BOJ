8d92fc22e90d772cc9170109614b8034f548b534
진주 <109266870+pearl313@users.noreply.github.com>
2024-03-22 00:46:34 +0900

    [Silver II] Title: 나무 자르기, Time: 252 ms, Memory: 130252 KB -BaekjoonHub

--- 백준/Silver/14247. 나무 자르기/나무 자르기.py
+++ 백준/Silver/14247. 나무 자르기/나무 자르기.py
@@ -5,11 +5,9 @@ n = int(input())
 tree = list(map(int, input().split()))
 height = list(map(int, input().split()))
 
-ls = [[] for _ in range(n)]
-for i in range(n):
-    ls[i] = [height[i], tree[i]]
-    
+ls = [[height[i], tree[i]] for i in range(n)] 
 ls.sort()
+
 total = 0
 for i in range(n):
     total += ls[i][1] + (ls[i][0] * i)
