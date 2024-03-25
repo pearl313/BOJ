73cb9deeaec7319e1e5ae5285cd11621d867405c
진주 <109266870+pearl313@users.noreply.github.com>
2024-03-26 00:45:33 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,22 +1,25 @@
 import sys
 input = sys.stdin.readline
 
-def DFS(v):
-    global cnt
-    cnt += 1
-    visited[v] = True
-    for i in graph[v]:
-        if not visited[i]:
-            DFS(i)
+n = int(input())
+graph = [[] for _ in range(n + 1)]
 
-com = int(input())
-N = int(input())
-graph = [[] for _ in range(com + 1)]
-visited = [False] * (com + 1)
-cnt = 0
-for _ in range(N):
+for _ in range(int(input())):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
-DFS(1)
-print(cnt - 1)
+
+visited = [False] * (n + 1)
+cnt = 0
+
+def dfs(x):
+    global cnt
+    visited[x] = True
+    for i in graph[x]:
+        if visited[i]:
+            continue
+        cnt += 1
+        dfs(i)
+
+dfs(1)
+print(cnt)
