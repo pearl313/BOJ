8ac0eb968464f20d9a61d0e5587789e2367dc700
진주 <109266870+pearl313@users.noreply.github.com>
2024-03-26 00:50:44 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 380 ms, Memory: 169480 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,41 +1,28 @@
 import sys
 input = sys.stdin.readline
 
-def find(x):
-    # 루트인 것!
-    if parent[x] == x:
-        return x
-    else:
-        parent[x] = find(parent[x])
-        return parent[x]
-
-def union(a, b):
-    a = find(a)
-    b = find(b)
-    if a == b:
-        return
-    # 크기 비교해서 더 작은 게 큰 쪽으로 붙기
-    if rank[a] < rank[b]:
-        parent[a] = b
-    elif rank[a] > rank[b]:
-        parent[b] = a
-    else:
-        parent[a] = b
-        rank[b] += 1
-
 n, m = map(int, input().split())
-parent = list(range(1000001))
-rank = list(0 for _ in range(1000001))
+
+graph = [[] for _ in range(n + 1)]
 for _ in range(m):
-    a, b = map(int, input().split())
-    union(a, b)
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
 
-visited = [False for i in range(n + 1)]
-cnt = 0
+visited = [False] * (n + 1)
 
-for i in range(1, n + 1):
-    x = find(i)
-    if not visited[x]:
-       cnt += 1
+def dfs(x):
     visited[x] = True
+    for i in graph[x]:
+        if visited[i]:
+            continue
+        dfs(i)
+
+cnt = 0
+for i in range(1, n + 1):
+    if visited[i]:
+        continue
+    dfs(i)
+    cnt += 1
+    
 print(cnt)
