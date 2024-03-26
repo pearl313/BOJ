c3d53e96583835f10951aa496553ec55c2ca7980
진주 <109266870+pearl313@users.noreply.github.com>
2024-03-26 19:31:14 +0900

    [Silver II] Title: 촌수계산, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -1,27 +1,27 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
-def BFS(a):
-    q = deque()
-    q.append(a)
-    visited[a] = 0
-    while q:
-        cur = q.popleft()
-        for i in graph[cur]:
-            if visited[i] != -1:
-                continue
-            q.append(i)
-            visited[i] = visited[cur] + 1
-
 n = int(input())
-a, b = map(int, input().split())
+s, e = map(int, input().split())
 m = int(input())
+
 graph = [[] for _ in range(n + 1)]
-visited = [-1] * (n + 1)
 for _ in range(m):
-    x, y = map(int, input().split())
-    graph[x].append(y)
-    graph[y].append(x)
-BFS(a)
-print(visited[b])
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited = [False] * (n + 1)
+def dfs(s, e, cnt):
+    if s == e:
+        print(cnt)
+        exit()
+        return cnt
+    visited[s] = True
+    for i in graph[s]:
+        if visited[i]:
+            continue
+        dfs(i, e, cnt + 1)
+
+dfs(s, e, 0)
+print(-1)
