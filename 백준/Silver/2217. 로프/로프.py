d49b9c58886f69356c308aa00c6a074b366206c7
진주 <109266870+pearl313@users.noreply.github.com>
2024-03-27 20:31:05 +0900

    [Silver IV] Title: 로프, Time: 152 ms, Memory: 113500 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.py
+++ 백준/Silver/2217. 로프/로프.py
@@ -2,8 +2,11 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-rope = sorted([int(input()) for _ in range(n)], reverse=True)
-ans = 0
+weight = list(int(input()) for _ in range(n))
+
+weight.sort(reverse=True)
+ans = -1e10
 for i in range(n):
-    ans = max(ans, rope[i] * (i + 1))
+    ans = max(ans, weight[i] * (i + 1))
+    
 print(ans)
