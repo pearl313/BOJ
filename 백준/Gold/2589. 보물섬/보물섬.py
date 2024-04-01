224404e571b69ffc8f81bb6bf7c6dd5488f3629c
진주 <109266870+pearl313@users.noreply.github.com>
2024-04-02 00:39:03 +0900

    [Gold V] Title: 보물섬, Time: 884 ms, Memory: 118620 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.py
+++ 백준/Gold/2589. 보물섬/보물섬.py
@@ -1,40 +1,44 @@
-import sys
+import sys, heapq
 input = sys.stdin.readline
 from collections import deque
 
-def possible(a, b):
-    return 0 <= a < sero and 0 <= b < garo
+l, w = map(int, input().split())
+arr = [input().strip() for _ in range(l)]
 
-dxy = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+def possible(a, b):
+    return 0 <= a < l and 0 <= b < w
+
+dxy = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 def bfs(x, y):
+    visited = [[-1] * w for _ in range(l)]
     visited[x][y] = 0
     q = deque()
     q.append((x, y))
+
     while q:
         cur_x, cur_y = q.popleft()
         for dx, dy in dxy:
             nx, ny = cur_x + dx, cur_y + dy
             if not possible(nx, ny):
                 continue
-            if treasure_map[nx][ny] == 'W':
-                continue
             if visited[nx][ny] != -1:
+                continue
+            if arr[nx][ny] == 'W':
                 continue
             visited[nx][ny] = visited[cur_x][cur_y] + 1
             q.append((nx, ny))
-    ans = 0
-    for i in range(sero):
-        temp = max(visited[i])
-        ans = max(ans, temp)
-    return ans
 
-sero, garo = map(int, input().split())
-treasure_map = list(input().strip() for _ in range(sero))
-treasure = 0
-for i in range(sero):
-    for j in range(garo):
-        if treasure_map[i][j] == 'L':
-            visited = [[-1] * garo for _ in range(sero)]
-            treasure = max(treasure, bfs(i, j))
-print(treasure)
+    max_val = -1e10
+    for i in visited:
+        max_val = max(max_val, max(i))
+    return max_val
+
+ans = -1e10
+for i in range(l):
+    for j in range(w):
+        if arr[i][j] == 'W':
+            continue
+        ans = max(ans, bfs(i, j))
+
+print(ans)
